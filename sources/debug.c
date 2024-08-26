bdcff5f7332df7a8ae0003b360bf08303765594e
mota494 <migueloureiromota@gmail.com>
2024-08-26 22:57:34 +0100

    thread

--- sources/debug.c
+++ sources/debug.c
@@ -6,7 +6,7 @@
 /*   By: miguel <miguel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 21:17:10 by miguel            #+#    #+#             */
-/*   Updated: 2024/08/26 21:19:15 by miguel           ###   ########.fr       */
+/*   Updated: 2024/08/26 21:20:35 by miguel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,7 @@ void    print_all(t_data *d, t_fork *f, t_philos *p)
     ft_printf("\n--------------------\n");
     while(i < d->n_philos)
     {
-        ft_printf("| [%d] & %p & %p |", p[i].p_id, p[i].lfork, 
-        p[i].rfork);
+        ft_printf("| [%d] & %p & %p |", p[i].p_id, p[i].lfork, p[i].rfork);
         i++;
     }
     ft_printf("\n--------------------\n");
--- sources/sim.c
+++ sources/sim.c
@@ -6,14 +6,40 @@
 /*   By: miguel <miguel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 21:35:51 by miguel            #+#    #+#             */
-/*   Updated: 2024/08/26 21:18:12 by miguel           ###   ########.fr       */
+/*   Updated: 2024/08/26 22:39:52 by miguel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
 
+void    one_philo(t_data *data)
+{
+    
+}
+
+void    simulation(t_data *data)
+{
+    
+}
+
+void    thread_create(t_data *data)
+{
+    int i;
+
+    i = 0;
+    while (i < data->n_philos)
+    {
+        pthread_create(&data->philo[i].thread, NULL, &simulation, data);
+        i++;
+    }
+}
+
 void    sim_start(t_data *data)
 {
     if (data->n_must_eat != -1 && data->n_must_eat <= 0)
         free_exit(data);
+    else if (data->n_philos == 1)
+        one_philo(data);
+    else
+        thread_create(data);
 }
